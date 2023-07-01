fa887d382d5bb49e18b1ca3f878b8063d985658a
Juan Fredes <juanjgfredes@gmail.com>
2023-07-01 17:22:47 -0300

    Modifica liquibase para produccion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn liquibase:update -Pprod
+mvn liquibase:update -Pprod mvn liquibase:update -Pprod -Denv.PROD_DB_HOST=$PROD_DB_HOST -Denv.PROD_DB_PORT=$PROD_DB_PORT -Denv.PROD_DB_NAME=$PROD_DB_NAME -Denv.PROD_DB_USERNAME=$PROD_DB_USERNAME -Denv.PROD_DB_PASSWORD=$PROD_DB_PASSWORD
 java -jar -Dspring.profiles.active=prod /target/escuela-0.0.1-SNAPSHOT.jar
